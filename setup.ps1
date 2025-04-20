c677d07753d8bd92550c67a2a6e276a76204cf51
R-Sh1ki <chengtao.yao@outlook.com>
2025-04-20 09:46:31 +0800

    feat: add starship config.

---
+++ setup.ps1
@@ -0,0 +1,10 @@
+winget.exe install --id Starship.Starship
+winget.exe install --id Microsoft.Powershell --source winget
+
+if (!(Test-Path $PROFILE)) { New-Item -Type File -Path $PROFILE -Force }
+Copy-Item .\Microsoft.PowerShell_profile.ps1 $PROFILE -Force
+
+$starshipConfPath = "$Env:HomePath\.config"
+
+if (!(Test-Path $starshipConfPath)) { New-Item -Type Directory -Path $starshipConfPath }
+Copy-Item -Path starship.toml -Destination $starshipConfPath
